596722a6742ae271aeca13af54c56ff172ea74ec
Yahya Kamran <yahya.kamran@seamless.se>
2023-03-18 22:35:38 +0500

    Updated the Exercise Details

--- src/Ninja.Exercise/Exercise01.go
+++ src/Ninja.Exercise/Exercise01.go
@@ -1,5 +1,20 @@
 package main
 
+/**
+
+Hands-on exercise #1
+	1. Using the short declaration operator, ASSIGN these VALUES to VARIABLES with the
+	IDENTIFIERS “x” and “y” and “z”
+	a. 42
+	b. “James Bond”
+	c. true
+	2. Now print the values stored in those variables using
+	a. a single print statement
+	b. multiple print statements
+	code: here’s the solution: https://play.golang.org/p/yYXnWXIQNa
+
+*/
+
 import "fmt"
 
 func main() {
